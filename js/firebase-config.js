7f66ecdb0f4aa8293d1074b6c6eb1312354c6648
채연 <woochaeyeon0220@gmail.com>
2026-06-12 17:12:44 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,14 @@
 // ============================================================
 // Firebase 설정 파일
-// Firebase 콘솔 > 프로젝트 설정 > 일반 > 내 앱 > SDK 설정 및 구성에서
-// 아래 값을 복사해서 붙여넣으세요.
+// Firebase 콘솔 > 프로젝트 설정 > 일반 > 내 앱 > SDK 설정 및 구성
 // ============================================================
 export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyARBYt0nzTaxyDq4Hc_C4OSrt6sPiXvZP4",
+  authDomain: "bbbb-5fa27.firebaseapp.com",
+  databaseURL: "https://bbbb-5fa27-default-rtdb.firebaseio.com",
+  projectId: "bbbb-5fa27",
+  storageBucket: "bbbb-5fa27.firebasestorage.app",
+  messagingSenderId: "753930418369",
+  appId: "1:753930418369:web:4b0909d99621ecd8e63c40",
+  measurementId: "G-6PREM62Z4K"
 };
